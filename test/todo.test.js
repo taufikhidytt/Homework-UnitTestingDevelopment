68cd26f218b308006076c1990983134053d9491a
Taufik Hidayat <hidytt.taufik@gmail.com>
2024-03-23 01:30:03 +0700

    update testing

--- test/todo.test.js
+++ test/todo.test.js
@@ -1,32 +1,18 @@
 const request = require('supertest');
 const app = require('../app');
 
+describe('POST /api/todo', function () {
+    it('insert todo data', function (done) {
+        request(app)
+            .post('/api/todo')
+            .send({
+                title: 'Data Testing'
+            })
+            .end(function (err, res) {
+                if (err) return done(err);
 
-describe('Todo Testing API', () => {
-    describe('/api/todo', () => {
-        it('should return new todo data', (done) => {
-            const data = {
-                "id": 11,
-                "title": "Data Testing",
-                "createdAt": "2024-03-21T14:40:59.997Z",
-                "updatedAt": "2024-03-21T14:40:59.997Z"
-            }
-
-            request(app)
-                .post('/api/todo')
-                .send(data)
-                .end((err, res) => {
-                    if (err) {
-                        done(err)
-                        return
-                    }
-                    console.log(res.body.title, "<<<<<<<<");
-                    expect(res.body.title).toEqual('Data Testing')
-                    expect(res.status).toEqual(201)
-
-                    done()
-                })
-
-        })
-    })
-})
+                expect(res.body.title).toEqual('Data Testing')
+                return done();
+            });
+    });
+});
